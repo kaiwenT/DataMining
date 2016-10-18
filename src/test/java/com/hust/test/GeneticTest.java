a940595467e2f15410d4ef29bf76314086bfe09e
gaoyan10 <gaoyan10@baidu.com>
2016-10-18 15:51:18 +0800

    更改遗传算法测试用例

--- src/test/java/com/hust/test/GeneticTest.java
+++ src/test/java/com/hust/test/GeneticTest.java
@@ -10,7 +10,7 @@ public class GeneticTest {
 
     @Before
     public void init() {
-        inputs = new int[] { 2, 42, 5, 3, 71, 5, 39, -11000, 8, 5, 6, 3, 3, 5, 3, -60000, 2, 3, 1 };
+        inputs = new int[] {42, 5, 3, 71, 5, 39, -11000, 8 };
     }
 
     @Test
